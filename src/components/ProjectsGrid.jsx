9af7e750ddfd8536fe6aae4f1e33da01d73952a4
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-04-14 22:03:11 +0200

    Added modal

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
+import ProjectModal from "./Modal/ProjectModal";
 
 
 const ProjectsGrid = ({filteredProjects}) => {
 
+  const [modalShow, setModalShow] = useState(false)
+  const [modalData, setModalData] = useState(null);
+
+
+  const handleClose = () => setModalShow(false);
+
+  const handleShow = (project) => {
+    setModalData(project);
+    setModalShow(true);
+  }
+
+
   return(
-    filteredProjects.map(project => (
+    <>
 
-      <motion.div className="image-card"
-                  key={project.id}
-                  layout
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{duration: 0.3 }}>
+      {filteredProjects.map(project => (
 
-        <img src={project.images[0]}
-             alt={project.title} />
-        <h2>{project.title}</h2>
+        <motion.div className="image-card"
+                    key={project.id}
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{duration: 0.3 }}
+                    onClick={() => handleShow(project)}>
 
-      </motion.div>
+          <img src={project.images[0]}
+               alt={project.title} />
+          <h2>{project.title}</h2>
 
-    ))
+        </motion.div>
+
+      ))}
+
+      {modalData &&
+        <ProjectModal show={modalShow} modalData={modalData} handleClose={handleClose} />
+      }
+
+    </>
   )
 }
 
