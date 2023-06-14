66846fe057729c92646d4f00c0dd647c47ff2344
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-06-15 00:00:24 +0200

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { HeroSection } from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
 import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <>
+      <NavBar />
       <HeroSection />
       <ProjectsSection/>
       <Footer />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { TwitterIcon, GithubIcon, LinkedInIcon} from './Icons'
+import { motion } from 'framer-motion'
+import Logo from './Logo'
+
+
+const NavBar = () => {
+
+    return (
+        <header className='w-[90%] py-4 font-medium relative z-10 m-auto
+                           flex flex-row items-baseline justify-between
+                           px-8 xs:px-16
+                           dark:text-light'>
+            
+            
+            <div className="flex flex-row items-baseline">
+                <div className='pr-4'>
+                    <Logo/>
+                </div>
+
+                <h1 className="text-3xl">
+                    Eduard Tiron - Full-stack Developer
+                </h1>
+            </div>
+
+            {/* Socials */}
+            <nav className='flex items-center justify-end '>
+                <motion.a href="https://linkedin.com/in/eduardtiron/?locale=en_US"
+                        target={"_blank"}
+                        className='w-6 mx-3'
+                        whileHover={{y:-2}}
+                        whileTap={{scale:0.9}}>
+                    <LinkedInIcon />
+                </motion.a>
+                <motion.a href="https://github.com/Tyronnosaurus"
+                        target={"_blank"}
+                        className='w-6 ml-3'
+                        whileHover={{y:-2}}
+                        whileTap={{scale:0.9}}>
+                    <GithubIcon />
+                </motion.a>
+            </nav>
+
+
+        </header>
+    )
+}
+
+export default NavBar
