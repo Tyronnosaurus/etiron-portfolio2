225d4acb9ef7c2233e1c9d35aebfb72ccdaf5d33
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-06-17 22:44:15 +0200

    Improved carousel thumbnails

--- src/components/ProjectsSection/ControlledCarousel.jsx
+++ src/components/ProjectsSection/ControlledCarousel.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from 'react-responsive-carousel';
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./ControlledCarousel.css";
 
 
 const ControlledCarousel = ({images}) => {
