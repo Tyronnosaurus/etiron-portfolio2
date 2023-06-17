995b71706ddbe37d69c769f2e28e739a55f5e71f
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-06-17 16:22:51 +0200

    Modal now uses Tailwind

--- src/components/ProjectsSection/Modal/ProjectModal.jsx
+++ src/components/ProjectsSection/Modal/ProjectModal.jsx
@@ -3,36 +3,6 @@ import ControlledCarousel from '../Carousel/ControlledCarousel';
 import Modal from 'react-modal';
 import { CloseX } from '../../Icons';
 
-import CloseIcon from '../../../assets/icons/Close.png'
-
-const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-
-      width: "800px",
-      maxHeight: "80%"
-    },
-    overlay: {
-        background: "rgba(1, 1, 1, 0.6)",
-    }
-  };
-
-
-
-// const CloseIcon = () => {
-//   return (
-//     <div className="flex flex-col justify-center items-center border">
-//         <span className="bg-dark block h-0.5 w-4 rounded-sm  rotate-45  translate-y-1"/>
-//         <span className="bg-dark block h-0.5 w-4 rounded-sm -rotate-45 -translate-y-1"/>
-//     </div>
-//   )
-// }
-
 
 
 const ProjectModal = ({modalData, show, handleClose}) => {
@@ -41,19 +11,22 @@ const ProjectModal = ({modalData, show, handleClose}) => {
 
         <Modal isOpen={show}
                onRequestClose={handleClose}
-               style={customStyles}
-               onClick={handleClose} 
-               className="">
+               onClick={handleClose}
+               className="absolute left-0 right-0 mx-auto top-16 w-[80%] max-w-[700px]
+                          border border-darkx rounded-xl outline-none
+                          p-5 overflow-auto
+                          bg-light"
+               overlayClassName="fixed inset-0 bg-dark bg-opacity-60">
             
 
-            <button className="absolute top-4 right-4" onClick={handleClose}>
+            <button className="absolute top-0 right-0" onClick={handleClose}>
                 <CloseX />
             </button>
 
             <h2>{modalData.title}</h2>
-                
+            
             {/* <ControlledCarousel images={modalData.images} /> */}
-            <img src={modalData.images[0]} alt={"Project"} className="m-auto"/>
+            <img src={modalData.images[0]} alt={"Project"} className="m-auto h-[350px] object-contain"/>
                 
             <div className="description" dangerouslySetInnerHTML={{__html: modalData.description}} />
 
