e529443497aa473903a4770dd9cfe1213671a41a
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-11-26 23:06:55 +0100

    Added indication to filter

--- src/components/ProjectsSection/ButtonFilters.jsx
+++ src/components/ProjectsSection/ButtonFilters.jsx
@@ -194,7 +194,8 @@ const ButtonFilters = ({projects, setFilteredProjects}) => {
         </div>
       </div>
 
-      {(activeFilter !== 'All') &&
+      {(activeFilter == 'All') ? 
+      <p>Click on any skill to show only related projects.</p> :
       <p>Showing all projects with the '{activeFilter}' tag:</p>}
 
     </div>
