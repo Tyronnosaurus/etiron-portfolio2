016fa1dd32f8890e6767cd05a684af294ec03b5d
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-12-24 11:42:31 +0100

    Added security question to download resume

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import AnimatedText from './AnimatedText'
 import ProfilePic from '../assets/me.jpg'
 
@@ -6,20 +6,61 @@ import ProfilePic from '../assets/me.jpg'
 
 export const HeroSection = () => {
 
+  const [showQuestion, setShowQuestion] = useState(false);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [questionAlreadyAnswered, setQuestionAlreadyAnswered] = useState(false)
+
+
+  // Open the security question to download resume (if it hasn't been solved already)
+  const handleOpenQuestion = () => {
+    if (!questionAlreadyAnswered)
+      setShowQuestion(true);
+    else
+      handleResumeDownload();
+  };
+
+  // Track answer written by user
+  const handleAnswerChange = (event) => {
+    setUserAnswer(event.target.value);
+  };
+
+
+  // Check if the user's answer is correct
+  const handleCheckAnswer = () => {  
+    if (userAnswer === '4') {
+      handleResumeDownload();
+      setShowQuestion(false);
+      setQuestionAlreadyAnswered(true);
+    } else {
+      alert('Incorrect answer. Please try again.');
+    }
+  };
+
+  // Download resume
+  const handleResumeDownload = () => {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');  // To open in new tab
+    link.href = '/resume/Resume Eduard Tiron.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+
   return (
     <div className="bg-light dark:bg-dark
                     p-8 md:p-12 lg:p-16 xl:p-24">
 
       <div className='items-center flex flex-col lg:flex-row'>
-          
+        
+        {/* Picture */}
         <div className='w-[70%] lg:w-1/2 mx-10'>
           <img src={ProfilePic} alt="portrait"
                 className='h-auto inline-block lg:w-full border-2' />
         </div>
 
 
-
-
+        {/* Description */}
         <div className='flex flex-col items-center self-center w-full lg:w-1/2'>
           
           <p className='my-4 font-medium w-full
@@ -49,22 +90,36 @@ export const HeroSection = () => {
           {/* Buttons */}
           <div className='flex items-center mt-2 self-center lg:self-start'>
 
-            <a href="/" target="_blank" rel="noreferrer"
+            <a href="mailto:Eduard.Tiron@gmail.com" target="_blank" rel="noreferrer"
+               className='flex items-center rounded-lg font-semibold
+                          p-2 px-4 md:p-2.5 md:px-6 mr-2
+                          text-sm md:text-base
+                          first-letter: bg-dark text-light hover:bg-light hover:text-dark
+                          border-2 border-solid border-dark hover:border-dark
+                          dark:bg-light dark:text-dark dark:border-light dark:hover:bg-dark dark:hover:text-light'>
+              Email me
+            </a>
+
+            <button href="/" target="_blank" rel="noreferrer"
               className='flex items-center rounded-lg font-semibold
                          p-2 px-4 md:p-2.5 md:px-6
                          text-sm md:text-base
                          bg-dark text-light hover:bg-light hover:text-dark
                          border-2 border-solid border-dark hover:border-dark
-                         dark:bg-light dark:text-dark dark:border-light dark:hover:bg-dark dark:hover:text-light'>
+                         dark:bg-light dark:text-dark dark:border-light dark:hover:bg-dark dark:hover:text-light'
+              onClick={handleOpenQuestion}>
               Resume
-            </a>
+            </button>
 
-            <a href="mailto:abcd@gmail.com" target="_blank" rel="noreferrer"
-              className='font-medium capitalize underline text-dark dark:text-light
-                         ml-2 xs:ml-4
-                         text-sm md:text-base'>
-              Email me
-            </a>
+            {showQuestion && (
+              <div className='flex flex-col items-center p-2'>
+                <label>What is 2+2?</label>
+                <input className='w-5' type="text" value={userAnswer} onChange={handleAnswerChange} />
+                <button className='font-bold' onClick={handleCheckAnswer}>Download</button>
+              </div>
+            )}
+
+
 
           </div>
 
